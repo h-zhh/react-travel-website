5410124e491e6c5d8715a10a71438bbcf2fcefb7
Zhihan Hu <89308955+h-zhh@users.noreply.github.com>
2023-03-17 22:59:15 +0100

    add cards to home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,11 +2,13 @@ import "../../App.css";
 import React from "react";
 import "../../App.css";
 import HomeSection from "../HomeSection";
+import Cards from "../Cards";
 
 function Home() {
   return (
     <>
       <HomeSection />
+      <Cards />
     </>
   );
 }
